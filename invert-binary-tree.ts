4db30aa598f424746ef49f13630db6c5b5a9be4b
Leo Massaroli <massaroli.leonardo@gmail.com>
2022-02-20 20:43:36 -0300

    resolve it

--- invert-binary-tree.ts
+++ invert-binary-tree.ts
@@ -5,5 +5,23 @@ export interface Tree {
 }
 
 export function invertBinaryTree(tree: Tree) {
-  return 'TODO';
+  const left = tree.left;
+  const right = tree.right;
+
+  // Invert the current branch
+  tree.left = right;
+  tree.right = left;
+
+  // If left branch has other branches then invert them too
+  if (left) {
+    invertBinaryTree(left);
+  }
+
+  // If right branch has other branches then invert them too
+  if (right) {
+    invertBinaryTree(right);
+  }
+
+  // Return the inverted tree
+  return tree;
 }
